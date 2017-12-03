aa410522c00f941ffb3e5c3d6d4bc3cd546f5f10
asus410 <fco.fedez.castro@gmail.com>
2017-12-03 19:14:22 +0100

    terminado java

--- app/src/main/java/com/example/asus410/mi_segunda_aplicacion/MainActivity.java
+++ app/src/main/java/com/example/asus410/mi_segunda_aplicacion/MainActivity.java
@@ -5,33 +5,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int cod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btninteres = (Button)findViewById(R.id.btnIntereses);
+        //Button btninteres = (Button)findViewById(R.id.btnIntereses);
+        cod = 0;
+    }
+
+    @Override
+    protected void onActivityResult(int request, int result, Intent i) {
+        if(request == 10 && result == RESULT_OK) cod =1;
     }
 
     public void clickIntereses(View view){
 
-        Intent i = new Intent(this,Intereses.class);
-        startActivity(i);
+        if(cod == 0){
+            Toast t = Toast.makeText(getApplicationContext(),"Debe registrarse primero",
+                    Toast.LENGTH_LONG);
+            t.show();
+        }else {
+            Intent i = new Intent(this,Intereses.class);
+            startActivity(i);
+        }
+
     }
 
     public void clickNoticias(View v){
-        Intent i = new Intent(this,Noticias.class);
-        startActivity(i);
+        if(cod == 0){
+            Toast t = Toast.makeText(getApplicationContext(),"Debe registrarse primero",
+                    Toast.LENGTH_LONG);
+            t.show();
+        }else {
+            Intent i = new Intent(this, Noticias.class);
+            startActivity(i);
+        }
     }
 
     public void clickHistoria(View v){
-        Intent i = new Intent(this,Historia.class);
-        startActivity(i);
+        if(cod == 0){
+            Toast t = Toast.makeText(getApplicationContext(),"Debe registrarse primero",
+                    Toast.LENGTH_LONG);
+            t.show();
+        }else {
+            Intent i = new Intent(this, Historia.class);
+            startActivity(i);
+        }
     }
     public void  clickRegistro(View v){
         Intent i = new Intent(this,Registro.class);
-        startActivity(i);
+        startActivityForResult(i,10);
     }
+
+
 }
--- app/src/main/java/com/example/asus410/mi_segunda_aplicacion/Registro.java
+++ app/src/main/java/com/example/asus410/mi_segunda_aplicacion/Registro.java
@@ -1,13 +1,62 @@
 package com.example.asus410.mi_segunda_aplicacion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
 
+    private EditText nombre, direccion, telefono, edad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        nombre = (EditText) findViewById(R.id.ETnombre);
+        direccion = (EditText) findViewById(R.id.ETdireccion);
+        telefono = (EditText) findViewById(R.id.ETtelefono);
+        edad = (EditText) findViewById(R.id.ETedad);
+
+    }
+
+    public void clickAceptar(View v){
+        if(nombre.getText().toString().isEmpty() &&
+                direccion.getText().toString().isEmpty() &&
+                telefono.getText().toString().isEmpty() &&
+                edad.getText().toString().isEmpty()){
+            Toast t = Toast.makeText(getApplicationContext(),"Deben estar rellenos todos los campos",
+                    Toast.LENGTH_LONG);
+            t.show();
+        }else if(nombre.getText().toString().isEmpty()){
+            Toast t = Toast.makeText(getApplicationContext(),"Tiene que rellenar nombre",
+                    Toast.LENGTH_LONG);
+            t.show();
+        }else if(direccion.getText().toString().isEmpty()){
+            Toast t = Toast.makeText(getApplicationContext(),"Tiene que rellenar direccion",
+                    Toast.LENGTH_LONG);
+            t.show();
+        }else if(edad.getText().toString().isEmpty()){
+            Toast t = Toast.makeText(getApplicationContext(),"Tiene que rellenar edad",
+                    Toast.LENGTH_LONG);
+            t.show();
+        }else if(telefono.getText().toString().isEmpty()){
+            Toast t = Toast.makeText(getApplicationContext(),"Tiene que rellenar telefono",
+                    Toast.LENGTH_LONG);
+            t.show();
+        }else {
+            Toast t = Toast.makeText(getApplicationContext(),"Registro completo",
+                    Toast.LENGTH_LONG);
+            t.show();
+
+            Intent i = new Intent();
+            setResult(RESULT_OK,i);
+            finish();
+        }
+    }
+
+    public void clickVolver(View v){
+        finish();
     }
 }
